41821baf15a2738f64049b3a81937df5ec10640e
GEORGY <goshana87@mail.ru>
2020-11-08 22:54:30 +0300

    registration page

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -4,9 +4,24 @@ import { NavLink } from 'react-router-dom';
 const Registration = () => {
     return (
         <div>
-            <NavLink to="/login">
-                Registration
-            </NavLink>
+               <div className="login-wrapper">
+                <div className="login">
+                    <div className="login-inputs">
+                        <h1>Зарегистрироваться</h1>
+                        <label htmlFor="name">name</label>
+                        <input id="name" type="text" />
+                        <label htmlFor="email">email</label>
+                        <input id="email" type="text" />
+                        <label htmlFor="password">password</label>
+                        <input type="password" />
+                        <button>Зарегистрироваться</button>
+                    </div>
+                </div>
+                <div className="registration">
+                    <span>У вас уже есть аккаунт?</span>
+                    <NavLink to="/login">Войти</NavLink>
+                </div>
+            </div>
         </div>
     );
 };
