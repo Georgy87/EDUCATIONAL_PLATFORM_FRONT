9f9cb53aeb18cd03cebb18039168f8705414777b
GEORGY <goshana87@mail.ru>
2020-12-25 01:20:24 +0300

    Login Page, redux form, inputs and validate ts

--- src/components/validate/validateInput.tsx
+++ src/components/validate/validateInput.tsx
@@ -1,10 +1,12 @@
-export const requireEmail = (value) => {
+
+export type ValidatorsType = (value: string) => string | undefined;
+export const requireEmail: ValidatorsType = (value) => {
     return value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
         ? "Invalid email address"
         : undefined;
 };
 
-export const minLength = (min) => (value) => {
+export const minLength = (min: number): ValidatorsType  => (value) => {
     return value && value.length < min
     ? `Must be ${min} characters or more`
     : undefined;
